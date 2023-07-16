c73a12e1716dde3f961afe33ba9bbffdc6ebea22
Artem Artamonov <artartamonov@ozon.ru>
2023-07-16 20:46:30 +0600

    working state

--- src/common/controller/telegram.rs
+++ src/common/controller/telegram.rs
@@ -3,6 +3,8 @@ use crate::common::model::request;
 use crate::common::service::definitioner::Definitioner;
 use crate::common::service::DefinitionerService;
 use std::error::Error;
+use std::sync::Arc;
+use teloxide::dispatching::dialogue::InMemStorage;
 use teloxide::prelude::*;
 
 const HELLO_MSG: &str =
@@ -14,59 +16,83 @@ Available commands:
 {word} - show word definition
 "#;
 
-pub struct Controller {
-    service: Definitioner,
+type MyDialogue = Dialogue<State, InMemStorage<State>>;
+type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
+
+#[derive(Clone, Default)]
+pub enum State {
+    #[default]
+    Start,
 }
 
-impl Controller {
-    pub async fn new(service: Definitioner) -> Controller {
-        Controller { service }
+pub async fn run(bot: Bot, service: Arc<Definitioner>) {
+    Dispatcher::builder(
+        bot,
+        Update::filter_message()
+            .enter_dialogue::<Message, InMemStorage<State>, State>()
+            .branch(dptree::case![State::Start].endpoint(handle_start)),
+    )
+    .dependencies(dptree::deps![InMemStorage::<State>::new(), service.clone()])
+    .enable_ctrlc_handler()
+    .build()
+    .dispatch()
+    .await;
+}
+
+pub async fn handle_start(
+    bot: Bot,
+    msg: Message,
+    dialogue: MyDialogue,
+    service: Arc<Definitioner>,
+) -> HandlerResult {
+    let chat_id = msg.chat.id.0;
+    let username = msg.chat.username();
+
+    let profile = Profile {
+        id: chat_id,
+        name: username.unwrap_or("").to_string(),
+    };
+
+    service.start(request::Start { profile }).await.unwrap();
+
+    bot.send_message(msg.chat.id, HELLO_MSG).await?;
+
+    Ok(())
+}
+
+pub async fn handle_help(
+    bot: Bot,
+    msg: Message,
+    dialogue: MyDialogue,
+    service: Arc<Definitioner>,
+) -> HandlerResult {
+    service.help(request::Help {}).await.unwrap();
+
+    bot.send_message(msg.chat.id, HELP_MSG).await?;
+
+    Ok(())
+}
+
+pub async fn handle_word_definition(
+    bot: Bot,
+    msg: Message,
+    dialogue: MyDialogue,
+    service: Arc<Definitioner>,
+    word: &str,
+) -> HandlerResult {
+    if let Some(def) = service
+        .word(request::WordDefinition {
+            word: word.to_owned(),
+        })
+        .await
+        .unwrap()
+        .description
+    {
+        bot.send_message(msg.chat.id, def.fmt()).await?;
+        return Ok(());
     }
 
-    pub async fn handle_start(&self, bot: Bot, msg: Message) -> Result<(), Box<dyn Error>> {
-        let chat_id = msg.chat.id.0;
-        let username = msg.chat.username();
+    bot.send_message(msg.chat.id, "Word not found").await?;
 
-        let profile = Profile {
-            id: chat_id,
-            name: username.unwrap_or("").to_string(),
-        };
-
-        self.service.start(request::Start { profile }).await?;
-
-        bot.send_message(msg.chat.id, HELLO_MSG).await?;
-
-        Ok(())
-    }
-
-    pub async fn handle_help(&self, bot: Bot, msg: Message) -> Result<(), Box<dyn Error>> {
-        self.service.help(request::Help {}).await?;
-
-        bot.send_message(msg.chat.id, HELP_MSG).await?;
-
-        Ok(())
-    }
-
-    pub async fn handle_word_definition(
-        &self,
-        bot: Bot,
-        msg: Message,
-        word: &str,
-    ) -> Result<(), Box<dyn Error>> {
-        if let Some(def) = self
-            .service
-            .word(request::WordDefinition {
-                word: word.to_owned(),
-            })
-            .await?
-            .description
-        {
-            bot.send_message(msg.chat.id, def.fmt()).await?;
-            return Ok(());
-        }
-
-        bot.send_message(msg.chat.id, "Word not found").await?;
-
-        Ok(())
-    }
+    Ok(())
 }
--- src/telegram/main.rs
+++ src/telegram/main.rs
@@ -1,9 +1,17 @@
 use std::{error::Error, process::exit, sync::Arc};
 
-use definitioner_bot::common::{
-    api, config::Config, controller::telegram::Controller, repository, service::definitioner,
-};
-use teloxide::prelude::*;
+use definitioner_bot::common::controller::telegram;
+use definitioner_bot::common::{api, config::Config, repository, service::definitioner};
+use teloxide::{dispatching::dialogue::InMemStorage, prelude::*};
+
+type MyDialogue = Dialogue<State, InMemStorage<State>>;
+type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
+
+#[derive(Clone, Default)]
+pub enum State {
+    #[default]
+    Start,
+}
 
 #[tokio::main]
 async fn main() {
@@ -18,38 +26,9 @@ async fn main() {
     let dictionary = api::dictionary::Dictionary::new();
 
     let repo = repository::Repository::new(&config).await;
-    let service = definitioner::Definitioner::new(repo, dictionary);
-    let controller = Arc::new(Controller::new(service).await);
+    let service = Arc::new(definitioner::Definitioner::new(repo, dictionary));
 
     let bot = Bot::new(&config.teloxide_token);
 
-    teloxide::repl(bot, move |bot, msg| {
-        handler(bot, msg, Arc::clone(&controller))
-    })
-    .await;
-}
-
-async fn handler(bot: Bot, msg: Message, controller: Arc<Controller>) -> ResponseResult<()> {
-    let returned: Result<(), Box<dyn Error>> = match msg
-        .text()
-        .unwrap_or("/help ")
-        .split(" ")
-        .next()
-        .unwrap_or("/help")
-        .to_lowercase()
-        .as_str()
-    {
-        "/start" => controller.handle_start(bot, msg).await,
-        "/help" => controller.handle_help(bot, msg).await,
-        word => controller.handle_word_definition(bot, msg, word).await,
-    };
-
-    match returned {
-        Ok(_) => {}
-        Err(e) => {
-            eprintln!("error: {}", e);
-        }
-    }
-
-    Ok(())
+    telegram::run(bot, service.clone()).await;
 }
